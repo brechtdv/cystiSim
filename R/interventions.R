d76b264cd4d314abaf9692eb49ed1cef9fcaad0a
Brecht Devleesschauwer <brechtdv@gmail.com>
2016-02-18 11:57:58 -0500

    fix subsetting bug in do_oxf_vac
    + version bump

--- R/interventions.R
+++ R/interventions.R
@@ -107,50 +107,56 @@ function(x, coverage, efficacy.oxf, efficacy.vac,
   ## randomize coverage over eligible pigs
   ## .. covered pigs receive both OXF and VAC
   ## .. hence perfect correlation between OXF and VAC coverage
-  is_treated <- rbinom(sum(id), 1, coverage) == 1
-  is_vaccinated <- is_treated
+  is_oxf <- rbinom(sum(id), 1, coverage) == 1
+  is_vac <- is_oxf
 
   ## randomize effective OXF treatment over covered pigs
-  is_treated[is_treated == 1] <-
-    rbinom(sum(id[is_treated]), 1, efficacy.oxf) == 1
+  is_oxf_eff <-
+    rbinom(sum(is_oxf), 1, efficacy.oxf) == 1
 
   ## randomize effective VAC treatment over covered pigs
-  is_vaccinated[is_vaccinated == 1] <-
-    rbinom(sum(id[is_vaccinated]), 1, efficacy.vac) == 1
+  is_vac_eff <-
+    rbinom(sum(is_vac), 1, efficacy.vac) == 1
 
   ## OXF
 
   ## generate immunity for treated positive pigs (mature & immature)
   ## .. if immunity is Inf (cf vaccination), no need to reset
   is_immune <-
-    is.finite(x$pig$immunity[id][is_treated]) &
-    (x$pig$cysti[id][is_treated] == 1 |
-     x$pig$cysti_immature[id][is_treated] == 1)
-  x$pig$immunity[id][is_treated][is_immune] <- immunity.oxf
+    is.finite(x$pig$immunity[id][is_oxf][is_oxf_eff]) &
+    (x$pig$cysti[id][is_oxf][is_oxf_eff] == 1 |
+     x$pig$cysti_immature[id][is_oxf][is_oxf_eff] == 1)
+  x$pig$immunity[id][is_oxf][is_oxf_eff][is_immune] <- immunity.oxf
 
   ## both mature and immature cysts die
   ## .. 'intensity' and 'time_since_infection' get set to zero
-  x$pig$cysti[id][is_treated] <- 0
-  x$pig$cysti_immature[id][is_treated] <- 0
-  x$pig$intensity[id][is_treated] <- 0
-  x$pig$time_since_infection[id][is_treated] <- 0
+  x$pig$cysti[id][is_oxf][is_oxf_eff] <- 0
+  x$pig$cysti_immature[id][is_oxf][is_oxf_eff] <- 0
+  x$pig$intensity[id][is_oxf][is_oxf_eff] <- 0
+  x$pig$time_since_infection[id][is_oxf][is_oxf_eff] <- 0
 
   ## VAC
 
   ## generate immunity for pigs that were vaccinated < XXX months before
   ## .. note that VAC immunity has precendence over OXF immunity
-  is_immune <- !is.na(x$pig$time_since_vaccination[id][is_vaccinated]) &
-               x$pig$time_since_vaccination[id][is_vaccinated] > 0
-  x$pig$immunity[id][is_vaccinated][is_immune] <- immunity.vac
+  is_immune <-
+    !is.na(x$pig$time_since_vaccination[id][is_vac][is_vac_eff]) &
+    x$pig$time_since_vaccination[id][is_vac][is_vac_eff] > 0
+  x$pig$immunity[id][is_vac][is_vac_eff][is_immune] <-
+    immunity.vac
 
   ## reset counter for pigs that were vaccinated > XXX months before
-  is_reset <- !is.na(x$pig$time_since_vaccination[id][is_vaccinated]) &
-               x$pig$time_since_vaccination[id][is_vaccinated] <= 0
-  x$pig$time_since_vaccination[id][is_vaccinated][is_reset] <- interval + 1
+  is_reset <-
+    !is.na(x$pig$time_since_vaccination[id][is_vac][is_vac_eff]) &
+    x$pig$time_since_vaccination[id][is_vac][is_vac_eff] <= 0
+  x$pig$time_since_vaccination[id][is_vac][is_vac_eff][is_reset] <-
+    interval + 1
 
   ## initiate 'time_since_vaccination' if first vaccination
-  is_first <- is.na(x$pig$time_since_vaccination[id][is_vaccinated])
-  x$pig$time_since_vaccination[id][is_vaccinated][is_first] <- interval + 1
+  is_first <-
+    is.na(x$pig$time_since_vaccination[id][is_vac][is_vac_eff])
+  x$pig$time_since_vaccination[id][is_vac][is_vac_eff][is_first] <-
+    interval + 1
 
   ## return updated 'cystiRun' object
   return(x)
